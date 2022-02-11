2c02116ec2f95bd2cadb716106191ebc9b694336
Joe Lignelli <jjlignelli@gmail.com>
2022-02-11 13:51:57 -0500

    remove unnec file

--- nessus-vuln-analysis-cw.py
+++
@@ -1,9 +0,0 @@
-from lxml import etree
-
-f = open('scanreport.nessus', 'r')
-xml_content = f.read()
-f.close()
-
-parz = etree.XMLParser(huge_tree=True)
-root = etree.fromstring(text=xml_content, parser=parz)
-for block in root:
